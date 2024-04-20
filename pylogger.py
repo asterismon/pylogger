19e4b7987fe570f7a4138068bc6de02657be8a89
Xiaohetun <2276589156@qq.com>
2024-04-21 00:06:37 +0800

    modified:   pylogger.py

--- pylogger.py
+++ pylogger.py
@@ -12,7 +12,7 @@ from rich.console import Console
 __VERSION__ = "2.1.7"
 
 class Logger:
-    class level:
+    class mode:#调整日志级别
         ALL = -100
         DEBUG = -10
         NORMAL = 0
@@ -35,8 +35,24 @@ class Logger:
         auto_highlight: bool = False,
         logfile_max: int = 30,
         kw_min: int = 7,
-        level: int = level.NORMAL,
+        mode: int = mode.NORMAL,
     ) -> None:
+        '''
+        初始化Logger
+
+        Args:
+            process: 进程显示开关. 默认为False.
+            thread: 线程显示开关. 默认为False.
+            objID: 进程、线程和函数的ID开关,适合调试时打开. 默认为False.
+            func_name: 显示函数名. 默认为True.
+            filename: 显示文件名. 默认为True.
+            line: 显示行数. 默认为True.
+            quiet: 控制台静默. 默认为False.
+            auto_highlight: rich自动语法高亮开关. 默认为False.
+            logfile_max: 保留的旧日志文件数目. 默认为30.
+            kw_min: Logger类型关键字长度. 默认为7.
+            level: Logger日志级别. 默认为level.NORMAL.
+        '''        
         self.process = process
         self.thread = thread
         self.objID = objID
@@ -45,16 +61,16 @@ class Logger:
         self.line = line
         self.logfile_max = logfile_max
         self.kw_min = kw_min
-        self.level = level
+        self.level = mode
         self.quiet = quiet
         self.auto_highlight = auto_highlight
-        self.queue = Queue()
+        self.queue:Queue = Queue()
         self.console = Console()
         self.logfileCleaner()
         Thread(target=self.__listener, name="logWriter", daemon=True).start()
         return
 
-    def message(
+    def note(
         self,
         _string: str,
         callback: Callable | None = None,
@@ -63,7 +79,7 @@ class Logger:
     ) -> None:
         if self.level <= 0:
             self.logger(
-                mode="message",
+                mode="note",
                 _string=_string,
                 fore_color="rgb(0,255,255)",
                 callback=callback,
@@ -157,15 +173,24 @@ class Logger:
                 kwargs=kwargs,
             )
 
-    def off(
+    def panic(
         self,
         _string: str,
         callback: Callable | None = None,
         *args,
         **kwargs,
     ) -> None:
-        self.setLevel(-100)
-
+        if self.level <= 50:
+            self.logger(
+                mode="panic",
+                _string=_string,
+                fore_color="rgb(255,255,0)",
+                back_color="rgb(200,0,0)",
+                callback=callback,
+                args=args,
+                kwargs=kwargs,
+            )
+    
     def logfileCleaner(self):
         if not os.path.exists("logs"):
             os.mkdir("logs")
@@ -179,8 +204,12 @@ class Logger:
         filename = (
             f" in {os.path.basename(using.f_code.co_filename)}" if self.filename else ""
         )
-        process = f" <Process {current_process().name}>" if self.process else ""
-        thread = f" <Thread {current_thread().name}>" if self.thread else ""
+        cur_process = current_process()
+        process_id = f"(ID {id(cur_process)})" if self.objID else ""
+        process = f" <Process {cur_process.name}{process_id}>" if self.process else ""
+        cur_thread = current_thread()
+        thread_id = f"(ID {id(cur_thread)})" if self.objID else ""
+        thread = f" <Thread {cur_thread.name}{thread_id}>" if self.thread else ""
         objID = f" (ID {id(using.f_code)})" if self.objID else ""
         func_name = f" <{using.f_code.co_name}{objID}>" if self.func_name else ""
         line = f" ,line {using.f_lineno}" if self.line else ""
@@ -230,5 +259,5 @@ class Logger:
                     f.write(log)
                     self.queue.task_done()
 
-    def setLevel(self, level: int):
+    def setLevel(self, level: int | mode):
         self.level = level
